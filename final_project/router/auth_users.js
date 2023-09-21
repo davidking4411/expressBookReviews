3334b57f5bb7d197ad944f8ff4678253bc6cd39e
David <davidking4411@gmail.com>
2023-09-20 22:43:08 -0700

    finished

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password match the one we have in records.
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "successfully logged in" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Thank you for your review" });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  return res.status(300).json({ message: "You have deleted your review" });
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,70 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "new user registered" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", async function (req, res) {
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get("/isbn/:isbn", async function (req, res) {
+  let output = [];
+  console.log(books);
+  for (const this_book in books) {
+    console.log(req.params.isbn);
+    console.log("this_book.isbn " + books[this_book].isbn);
+    if (books[this_book].isbn == req.params.isbn) output.push(books[this_book]);
+  }
+  return res.status(300).json(output);
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", async function (req, res) {
+  let output = [];
+  console.log(books);
+  for (const this_book in books) {
+    console.log(req.params.author);
+    console.log("this_book.author " + books[this_book].author);
+    if (books[this_book].author.localeCompare(req.params.author) == 0)
+      output.push(books[this_book]);
+  }
+  return res.status(300).json(output);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let output = [];
+  console.log(books);
+  for (const this_book in books) {
+    console.log(req.params.title);
+    console.log("this_book.title " + books[this_book].title);
+    if (books[this_book].title.localeCompare(req.params.title) == 0)
+      output.push(books[this_book]);
+  }
+  return res.status(300).json(output);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let output = [];
+  console.log(books);
+  for (const this_book in books) {
+    console.log(req.params.isbn);
+    console.log("this_book.isbn " + books[this_book].isbn);
+    if (books[this_book].isbn == req.params.isbn)
+      output.push(books[this_book].reviews);
+  }
+  return res.status(300).json(output);
 });
 
 module.exports.general = public_users;
